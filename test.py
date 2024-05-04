c1f6a9826d7eece316dab0bc49fb570c21a0df52
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-05-04 16:52:59 +0530

    data updated

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import joblib
 
 # Load the saved model
@@ -20,7 +21,7 @@ def predict_crop(environmental_conditions):
     return prediction
 
 # Test the model with some example environmental conditions
-example_environment = [85, 60, 40, 25, 70, 7, 200]
+example_environment = [90,42,43,20.87974371,82.00274423,6.502985292,202.9355362,175,1712.196283,5.317803945]
 
 predicted_crop = predict_crop(example_environment)
 print("Predicted Crop:", predicted_crop)
